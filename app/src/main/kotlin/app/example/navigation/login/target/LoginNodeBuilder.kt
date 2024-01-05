f0ae6cf0f45c85751842fbc71e2eec0f436157e3
itsandreramon <thielea@th-brandenburg.de>
2024-01-05 22:29:58 +0100

    remove line

--- app/src/main/kotlin/app/example/navigation/login/target/LoginNodeBuilder.kt
+++ app/src/main/kotlin/app/example/navigation/login/target/LoginNodeBuilder.kt
@@ -2,7 +2,6 @@ package app.example.navigation.login.target
 
 import app.example.ui.screens.login.LoginViewModel
 import app.example.ui.util.ViewModelStoreOwnerProvider
-import com.bumble.appyx.navigation.builder.Builder
 import com.bumble.appyx.navigation.builder.SimpleBuilder
 import com.bumble.appyx.navigation.modality.BuildContext
 import com.bumble.appyx.navigation.node.Node

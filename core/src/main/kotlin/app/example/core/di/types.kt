3736669aa36c0a173627cab013c922d47512ff2b
itsandreramon <thielea@th-brandenburg.de>
2024-01-06 11:44:41 +0100

    remove unused code

--- core/src/main/kotlin/app/example/core/di/types.kt
+++ core/src/main/kotlin/app/example/core/di/types.kt
@@ -3,7 +3,3 @@ package app.example.core.di
 typealias DaggerSet<T> = @JvmSuppressWildcards Set<T>
 
 typealias InitializerFunction = () -> @JvmSuppressWildcards Unit
-
-interface DaggerComponentOwner {
-  val daggerComponent: Any
-}
